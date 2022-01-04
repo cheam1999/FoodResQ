13023bc091bfa53dfab83e873fa16104d9e7f1b8
yuchein <cheam.yuchein@gmail.com>
2022-01-04 23:06:40 +0800

    change in profile

--- FoodResQ/foodresq/lib/screen/profile.dart
+++ FoodResQ/foodresq/lib/screen/profile.dart
@@ -1,8 +1,11 @@
 import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:foodresq/component/normal_text.dart';
 import 'package:foodresq/constants/colour_constant.dart';
+import 'package:foodresq/utilities/size_config.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ionicons/ionicons.dart';
 
 class ProfileScreen extends HookConsumerWidget {
   static String routeName = "/profile";
@@ -32,10 +35,127 @@ class ProfileScreen extends HookConsumerWidget {
       body: SafeArea(
         child: LayoutBuilder(builder: (context, constraint) {
           return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(
-                "Profile Page",
-                style: TextStyle(fontSize: 50),
+              SizedBox(
+                height: getProportionateScreenHeight(30),
+              ),
+              Container(
+                  //padding:,
+                  width: double.infinity,
+                  // height: getProportionateScreenHeight(200),
+                  // decoration: BoxDecoration(color: Colors.white),
+                  constraints: BoxConstraints(
+                    minHeight: getProportionateScreenHeight(125),
+                  ),
+                  child: CircleAvatar(
+                    radius: 30.0,
+                    child: ClipOval(
+                      child: Image.asset('assets/graphics/profile.jpg'),
+                    ),
+                  )),
+              SizedBox(height: getProportionateScreenHeight(20)),
+              NormalText(
+                text: 'John Doe',
+                fontSize: 25,
+                isBold: true,
+              ),
+              SizedBox(height: getProportionateScreenHeight(5)),
+              NormalText(
+                text: 'test@test.com',
+                fontSize: 15,
+              ),
+              SizedBox(height: getProportionateScreenHeight(15)),
+              Container(
+                color: Colors.white,
+                width: getProportionateScreenWidth(300),
+                height: getProportionateScreenHeight(150),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            style: DefaultTextStyle.of(context).style,
+                            children: const <TextSpan>[
+                              TextSpan(
+                                text: 'You have saved ',
+                                style: TextStyle(
+                                  color: ColourConstant.kTextColor2,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              TextSpan(
+                                  text: '\n     20 ',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: ColourConstant.kButtonColor,
+                                    fontSize: 40,
+                                  )),
+                              TextSpan(
+                                  text: '\n    ingredients',
+                                  style: TextStyle(
+                                    color: ColourConstant.kTextColor2,
+                                    fontSize: 20,
+                                  )),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: getProportionateScreenHeight(15)),
+              Container(
+                color: Colors.white,
+                width: getProportionateScreenWidth(300),
+                height: getProportionateScreenHeight(150),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            style: DefaultTextStyle.of(context).style,
+                            children: const <TextSpan>[
+                              TextSpan(
+                                text: 'You have wasted ',
+                                style: TextStyle(
+                                  color: ColourConstant.kTextColor2,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              TextSpan(
+                                  text: '\n     20 ',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: ColourConstant.kButtonColor,
+                                    fontSize: 40,
+                                  )),
+                              TextSpan(
+                                  text: '\n    ingredients',
+                                  style: TextStyle(
+                                    color: ColourConstant.kTextColor2,
+                                    fontSize: 20,
+                                  )),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           );
